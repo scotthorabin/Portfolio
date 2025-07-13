986f6df3e0c6ec2a1423bbd282eca101c2b52037
Scott Horabin <scotthorabin@outlook.com>
2025-07-13 22:56:42 +0100

    began projects section

--- html/JS/script.js
+++ html/JS/script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+     window.addEventListener('load', () => {
+      const loader = document.getElementById('loader');
+
+      /* keep spinner visible for 1 second */
+      setTimeout(() => {
+        loader.classList.add('fade-out');
+        loader.addEventListener('transitionend', () => loader.remove());
+        document.body.classList.remove('preload');
+      }, 1000);           // 1000 ms = 1 s
+    });
